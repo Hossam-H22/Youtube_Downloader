100b5be93a6fb5413704ab2e7716370cbb45bf9f
Hossam Hatem <hosam.hatem222@gmail.com>
2024-08-04 19:16:06 +0300

    upload v1.1.1

--- main.py
+++ main.py
@@ -14,31 +14,57 @@ def clean_filename(filename):
     pattern = r'[<>:"/\\|?*\x00-\x1F]'
     return re.sub(pattern, '', filename)
 
-def get_youtube_video_info(video_url):
+def get_youtube_video_info(url):
     ydl_opts = {
         'quiet': True  # Suppress output
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(video_url, download=False)
-        return info
+        info = ydl.extract_info(url, download=False)
 
-def download_youtube_videos(playlist_url, noplaylist = True, output_path='.', numerate=False):
+    video_information = {
+        "url": url,
+        "id": info["id"],
+        "title": clean_filename(info["title"]),
+        "length": format_video_length(info["duration"]),
+        "length_seconds": info["duration"],
+        "description": info["description"],
+        "chapters": info.get('chapters', [])
+    }
+    return video_information
+
+def get_youtube_playlist_info(url):
+    playlist = Playlist(url)
+    video_urls = playlist.video_urls
+    playlist_information = {
+        'url': url,
+        'id': playlist.playlist_id,
+        'title': clean_filename(playlist.title),
+        'number_videos': playlist.length,
+        'length': '',
+        # 'description': playlist.description,
+        'transcript_list': [],
+        'videos_urls': video_urls,
+        'videos_info': [],
+    }
+
+    for video_url in video_urls:
+        playlist_information['videos_info'].append(get_youtube_video_info(video_url))
+
+    total_length_in_seconds = get_total_length_playlist(playlist_information['videos_info'])
+    playlist_information['length'] = format_video_length(total_length_in_seconds)
+    return playlist_information
+
+def download_youtube_videos(url, title, output_path='.'):
     ydl_opts = {
-        'outtmpl': f'{output_path}/%(playlist_index)s. %(title)s.%(ext)s' if not noplaylist and numerate else f'{output_path}/%(title)s.%(ext)s',
-        # 'format': 'best',  # Download the best quality
-        # 'format': 'bestvideo+bestaudio/best',  # Download the best quality
+        'outtmpl': f'{output_path}/{title}.%(ext)s',
         'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]',  # Download the best quality
-        'noplaylist': noplaylist,  # Only download single video, not the whole playlist
-        'progress_hooks': [lambda d: clean_filename(d['filename'])]  # Hook to sanitize filename
+        'noplaylist': True,  # Only download single video, not the whole playlist
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([playlist_url])
+        ydl.download([url])
 
-def download_subtitles(video_url, output_path='.', language_code='en', video_index=""):
+def download_subtitles(video_id, title, output_path='.', language_code='en'):
     try:
-        yt = YouTube(video_url)
-        # video_id = video_url.split('v=')[1] # Extract video ID from the URL
-        video_id = yt.video_id # Extract video ID from the URL
         transcript_list = YouTubeTranscriptApi.list_transcripts(video_id) # Get the transcript for the video
         transcript = transcript_list.find_transcript([language_code]) # Find the transcript in the desired language
         transcript_data = transcript.fetch() # Fetch the transcript
@@ -48,8 +74,7 @@ def download_subtitles(video_url, output_path='.', language_code='en', video_ind
         srt_text = formatter.format_transcript(transcript_data)
 
         # Save the subtitle as SRT file
-        title = clean_filename(yt.title)
-        filename = f'{output_path}/{title}.srt' if len(video_index)==0 else f'{video_index}{output_path}/{title}.srt'
+        filename = f'{output_path}/{title}.srt'
         with open(filename, "w", encoding="utf-8") as file:
             file.write(srt_text)
         print(f"Subtitle saved as {filename}")
@@ -65,7 +90,6 @@ def get_available_subtitles(video_id):
         subtitle_languages = []
         for transcript in transcripts:
             subtitle_languages.append(transcript.language_code) # Collect available languages
-
         return subtitle_languages
     except Exception as e:
         print(f"An error occurred: {e} \n")
@@ -102,11 +126,10 @@ def print_subtitles(transcript_list):
             subtitle_choise = 1
     return subtitle_choise
 
-def get_totlal_length_playlist(playlist_urls):
+def get_total_length_playlist(videos_info):
     total_length = 0
-    for video_url in playlist_urls:
-        yt = YouTube(video_url)
-        total_length += yt.length
+    for video in videos_info:
+        total_length += video['length_seconds']
     return total_length
 
 def clear_console():
@@ -164,59 +187,53 @@ def split_video_into_chapters(video_path, chapters, output_path):
         subprocess.run(command)
         index += 1
 
-def create_text_file(text, path):
+def create_text_file(list, path):
     os.makedirs(path, exist_ok=True) # Ensure the directory exists
     file_path = os.path.join(path, 'Link.txt') # Define the file path
 
     # Write the description to the file
-    with open(file_path, 'w') as file:
-        file.write(text)
-
-
+    with open(file_path, 'w', encoding='utf-8') as file:
+        for line in list:
+            file.write(line)
 
 def video_processes(video_url):
-    yt = YouTube(video_url)
-    video_title = yt.title
-    video_length = format_video_length(yt.length)
-    transcript_list = get_available_subtitles(yt.video_id)
-    if transcript_list[0] == "Error": transcript_list = []
+    info = get_youtube_video_info(video_url)
+    info['transcript_list'] = get_available_subtitles(info['id'])
+    if len(info['transcript_list'])>0 and info['transcript_list'][0] == "Error":
+        info['transcript_list'] = []
 
     print('           ', end='\r')
-
     print(f'\nVideo Information:')
-    print(f'Title: {video_title}')
-    print(f'Duration: {video_length}')
-    if len(transcript_list) > 0:
-        print(f'Video have subtitles: {transcript_list}')
+    print(f'Title: {info['title']}')
+    print(f'Duration: {info['length']}')
+    if len(info['transcript_list']) > 0:
+        print(f'Video have subtitles: {info['transcript_list']}')
     else:
         print(f"Video doesn't have subtitles")
 
     downloadChoice = input("\nDownload Video: Y or N ?  ")
     if downloadChoice == 'Y' or downloadChoice == 'y':
-        subtitle_choise = print_subtitles(transcript_list)
+        subtitle_choise = print_subtitles(info['transcript_list'])
         folder_path = input("\nPlease enter the path to the folder where you want to save: ")
         createVideoFolder = input("\nWrap video with a folder: Y or N ?  ")
         if createVideoFolder == 'Y' or createVideoFolder == 'y':
             createVideoFolder = True
-            folder_path = os.path.join(folder_path, clean_filename(video_title))
+            folder_path = os.path.join(folder_path, info['title'])
             os.makedirs(folder_path, exist_ok=True)
         else:
             createVideoFolder = False
 
         print("\nStart Downloading ... \n")
-        download_youtube_videos(video_url, True, folder_path)
+        download_youtube_videos(info['url'], info['title'], folder_path)
         subtitleFilePath = ""
-        if len(transcript_list) > 0 and subtitle_choise <= len(transcript_list):
-            subtitleFilePath = download_subtitles(video_url, folder_path, transcript_list[subtitle_choise - 1])
+        if len(info['transcript_list']) > 0 and subtitle_choise <= len(info['transcript_list']):
+            subtitleFilePath = download_subtitles(info['id'], info['title'], folder_path, info['transcript_list'][subtitle_choise - 1])
 
         if createVideoFolder:
-            # txt = f"{video_url} \n\n\n\n\n\n\n{video_description}"
-            create_text_file(video_url, folder_path)
-            info = get_youtube_video_info(video_url)
-            title = clean_filename(info['title'])
-            chapters = info.get('chapters', [])
+            create_text_file([info['url'], "\n\n\n\n\n\n\n\n\n\n\n", info['description']], folder_path)
+            video_path = os.path.join(folder_path, f"{info['title']}.mp4")
+            chapters = info['chapters']
             if chapters:
-                video_path = os.path.join(folder_path, f"{title}.mp4")
                 Chapters_folder_path = os.path.join(folder_path, 'Chapters')
                 os.makedirs(Chapters_folder_path, exist_ok=True)
                 split_video_into_chapters(video_path, chapters, Chapters_folder_path)
@@ -225,92 +242,78 @@ def video_processes(video_url):
 
         print("\nDownload Finished\n\n")
         os.startfile(folder_path)
-        # time.sleep(5)
-    # else: time.sleep(1)
 
 def playlist_processes(playlist_url):
-    playlist = Playlist(playlist_url)
-    playlist_title = playlist.title
-    playlist_urls = playlist.video_urls
-    playlist_length = format_video_length(get_totlal_length_playlist(playlist_urls))
-    transcript_list = []
-    # transcript_list = get_available_subtitles(playlist_urls[0].split('v=')[1])
-    for video_url in playlist_urls:
-        list = get_available_subtitles(video_url.split('v=')[1])
-        if list[0] != "Error":
-            transcript_list = list
+    info = get_youtube_playlist_info(playlist_url)
+    for video in info['videos_info']:
+        list = get_available_subtitles(video['id'])
+        if len(list)>0 and list[0] != "Error":
+            info['transcript_list'] = list
             break
 
     print('           ', end='\r')
-
     print(f'Playlist Information:')
-    print(f'Title: {playlist_title}')
-    print(f'Number of Videos: {len(playlist_urls)}')
-    print(f'Total Duration: {playlist_length}')
-    if len(transcript_list) > 0:
-        print(f'Playlist has subtitles: {transcript_list}')
+    print(f'Title: {info['title']}')
+    print(f'Number of Videos: {info['number_videos']}')
+    print(f'Total Duration: {info['length']}')
+    if len(info['transcript_list']) > 0: print(f'Playlist has subtitles: {info['transcript_list']}')
+    # print(f'Description: {info['description']}')
 
     downloadChoice = input("\nDownload Playlist: Y or N ?  ")
     if downloadChoice == 'Y' or downloadChoice == 'y':
-        subtitle_choise = print_subtitles(transcript_list)
+        subtitle_choise = print_subtitles(info['transcript_list'])
 
         numerateChoice = input("\nNumerated Playlist: Y or N ?  ")
-        if numerateChoice == 'y' or numerateChoice == 'Y':
-            numerateChoice = True
-        else:
-            numerateChoice = False
+        if numerateChoice == 'y' or numerateChoice == 'Y': numerateChoice = True
+        else: numerateChoice = False
 
         folder_path = input("\nPlease enter the path to the folder where you want to save: ")
 
         print("\nStart Downloading ... \n")
-        folder_path = os.path.join(folder_path, playlist_title)
+        folder_path = os.path.join(folder_path, info['title'])
         os.makedirs(folder_path, exist_ok=True)
 
-        download_youtube_videos(playlist_url, False, folder_path, numerateChoice)
         index = 1
-        for video_url in playlist_urls:
-            video_index = ""
-            if numerateChoice: video_index = format_counter(index, len(playlist_urls))
-            download_subtitles(video_url, folder_path, transcript_list[subtitle_choise - 1], video_index)
+        for video in info['videos_info']:
+            if numerateChoice: video_title = f'{format_counter(index, info['number_videos'])}{video['title']}'
+            else: video_title = video['title']
+
+            download_youtube_videos(video['url'], video_title, folder_path)
+            download_subtitles(video['id'], video_title, folder_path, info['transcript_list'][subtitle_choise - 1])
             index += 1
 
-        create_text_file(playlist_url, folder_path)
+        create_text_file([ info['url'] ], folder_path)
         print("\nDownload Finished\n\n")
         os.startfile(folder_path)
-        # time.sleep(5)
-    # else: time.sleep(1)
+
 
 def start_program():
     while True:
         print("\nWelcome to Youtube Downloader 😊 \n")
-
-        dwonloadType = int(input("Please choose number: \n1 - Video \n2 - Playlist \n3 - Quit 👋\nYou choice is: "))
-
-        if dwonloadType == 1:
+        downloadType = int(input("Please choose number: \n1 - Video \n2 - Playlist \n3 - Quit 👋\nYou choice is: "))
+        if downloadType == 1:
             video_url = input("\nPlease enter the link of youtube video: ")
-        elif dwonloadType == 2:
+            print('\n')
+            print('Waiting ...', end='\r')
+            video_processes(video_url)
+        elif downloadType == 2:
             playlist_url = input("\nPlease enter the link of youtube playlist: ")
+            print('\n')
+            print('Waiting ...', end='\r')
+            playlist_processes(playlist_url)
         else:
             break
 
-        print('\n')
-        print('Waiting ...', end='\r')
-
-        if dwonloadType == 1:
-            video_processes(video_url)
-        else:
-            playlist_processes(playlist_url)
-
-        z = input("\n\n\nPress any key to continue .. \n")
+        input("\n\n\nPress any key to continue .. \n")
         clear_console()
 
 
 
 def split_downloaded_video_and_subtitle_into_chapters():
     url = input("video url: ")
-    chapters = get_youtube_video_info(url).get('chapters', [])
-    if chapters:
-        print(f"Num of chapters: {len(chapters)} \n")
+    info = get_youtube_video_info(url)
+    if info['chapters']:
+        print(f"Num of chapters: {len(info['chapters'])} \n")
         folder_path = input("folder path: ")
         if folder_path.startswith('"'): folder_path = folder_path[1:]
         if folder_path.endswith('"'): folder_path = folder_path[:-1]
@@ -322,9 +325,9 @@ def split_downloaded_video_and_subtitle_into_chapters():
         if subtitleFilePath.endswith('"'): subtitleFilePath = subtitleFilePath[:-1]
         Chapters_folder_path = os.path.join(folder_path, 'Chapters')
         os.makedirs(Chapters_folder_path, exist_ok=True)
-        split_video_into_chapters(video_path, chapters, Chapters_folder_path)
+        split_video_into_chapters(video_path, info['chapters'], Chapters_folder_path)
         if len(subtitleFilePath) > 0:
-            split_subtitles_into_chapters(subtitleFilePath, chapters, Chapters_folder_path)
+            split_subtitles_into_chapters(subtitleFilePath, info['chapters'], Chapters_folder_path)
 
 
 if __name__ == '__main__':
